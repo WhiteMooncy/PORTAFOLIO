81c8258fec66f6009add7c5d7c25e5c4329df791
Mateo Villablanca <167587307+WhiteMooncy@users.noreply.github.com>
2025-11-05 18:46:27 -0300

    mayonesa update fixed all

---
+++ assets/js/slideInLeft.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const helloTitle = document.querySelector('.Hello-title');
+    if(helloTitle) {
+        helloTitle.classList.add('slideInLeft-animation');
+    }
+});
